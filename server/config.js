7b3941425a8202c9119db158602c2cc63edf3c71
anushaladha04 <anushaladha04@gmail.com>
2025-12-02 10:18:25 -0800

    server configs updated to support google oauth

--- server/config.js
+++ server/config.js
@@ -6,5 +6,6 @@ export const config = {
   port: process.env.PORT || 5050,
   mongoUri: process.env.MONGODB_URI || "",
   jwtSecret: process.env.JWT_SECRET || "change-me-in-prod",
+  googleClientId: process.env.GOOGLE_CLIENT_ID || "",
 };
 
