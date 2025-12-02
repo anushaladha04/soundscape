4761030d2c41e9069b38677512e478496efd9348
anushaladha04 <anushaladha04@gmail.com>
2025-12-02 10:42:36 -0800

    updated user model password to be required, min length of 8, + contain special character

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String },
+    password: { type: String, required: true, minlength: 8, specialCharacter: true},
     googleId: { type: String },
   },
   { timestamps: true }
