ee75159b7dd83a730b56e263e2b5088729142be2
anushaladha04 <anushaladha04@gmail.com>
2025-12-03 07:01:10 -0800

    updating server routes and user schema

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import authRoutes from "./auth.js";
-<<<<<<< HEAD
 import eventRoutes from "./events.js";
-=======
 import { getRecommendedEvents } from "../controllers/recommendationsController.js";
 import { requireAuth } from "../middleware/authMiddleware.js";
->>>>>>> anusha/auth
 
 const router = express.Router();
 
@@ -14,10 +11,7 @@ router.get("/health", (req, res) => {
 });
 
 router.use("/auth", authRoutes);
-<<<<<<< HEAD
 router.use("/events", eventRoutes);
-=======
 router.get("/events/recommendations", requireAuth, getRecommendedEvents);
->>>>>>> anusha/auth
 
 export default router;
